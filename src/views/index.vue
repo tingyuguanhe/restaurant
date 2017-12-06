1f316501003c54ccff10e48a06b9322c7ed3e540
ghtf <gfdgdgd@re.com>
2017-12-06 19:14:50 +0800

    tpl

--- src/views/index.vue
+++ src/views/index.vue
@@ -85,6 +85,20 @@
                 </div>
             </div>
       </section>
+      <footer class="footer">
+          <div class="erwei_code">
+              <img src="../../static/wechat.jpeg" width="100%" alt="">
+          </div>
+          <ul class="clearfix">
+              <li><a href="javascript:;">关于我们</a></li>
+              <li><a href="javascript:;">友情链接</a></li>
+              <li><a href="javascript:;">联系我们</a></li>
+          </ul>
+          <div class="copy_right">
+              <span>@2017 北京北京北京北京北京北京北京北京</span>
+              <span>All right reserved</span> 
+         </div>
+      </footer>
       
   </div>
 </template>
@@ -245,5 +259,34 @@ export default {
     }
    
 }
+
+.footer{
+    .erwei_code{
+        margin: 50px auto;
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background-color: #f0f;
+        overflow: hidden;
+    }
+    ul{
+        width: 50%;
+        margin: 0 auto;
+        li{
+            width: 33.33%;
+            height: 60px;
+            line-height: 60px;
+            font-size: 14px;
+            font-weight: 600;
+        }
+    }
+    .copy_right{
+        padding: 20px 0 40px 0;
+        span{
+            margin-right: 30px 0;
+        }
+    }
+}
+
 </style>
 
