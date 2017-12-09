badde6bc21d3c313db29a53d87f87724d9844d33
ghtf <gfdgdgd@re.com>
2017-12-09 21:24:17 +0800

    list

--- src/views/layout/bottom.vue
+++ src/views/layout/bottom.vue
@@ -12,8 +12,39 @@
             <span>&copy;2017 北京北京北京北京北京北京北京北京</span>
             <span>All right reserved</span> 
         </div>
+        <div class="back_top" v-if="back_top_show" @click="to_top">
+            <i class="el-icon-upload2"></i>
+        </div>
+
     </footer>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                back_top_show: false
+            }
+        },
+        mounted () {
+            window.addEventListener('scroll', this.handleScroll)
+        },
+        methods:{
+            handleScroll () {
+                var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+                //console.log(scrollTop)
+                if (scrollTop > 600) {
+                    this.back_top_show = true
+                } else {
+                    this.back_top_show = false
+                }
+            },
+            to_top(){
+                document.documentElement.scrollTop = 0;
+            }
+        }
+    }
+</script>
 
 <style lang="scss">
 @import url("../../style/common.css");
@@ -44,6 +75,19 @@
             color:#b1b1b1;
             font-size: 13px;
         }
+    }
+    .back_top{
+        position: fixed;
+        bottom: 12%;
+        right: 20px;
+        width: 45px;
+        height: 45px;
+        line-height: 45px;
+        background-color: #444444;
+        text-align: center;
+        font-size: 30px;
+        color: #aaa;
+        cursor:pointer;
     }
 }
 
